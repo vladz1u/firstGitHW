b18b2378c6baf9c6b1d5d0d5e3ec52b3233ae8af
Vladyslav Pudov <vladpudov2009@gmail.com>
2024-04-30 10:12:46 +0200

    second task

--- src/hw.java
+++ src/hw.java
@@ -1,11 +1,22 @@
 public class hw {
     public static void main(String[] args) {
-        firstTask(1);
+//        firstTask(1);
+        secondTask(-1);
+
     }
+
     public static void firstTask(int a) {
+        if (a == 0){
+            System.out.println("Correct");
+        } else
+            System.out.println("Incorrect");
+    }
+
+    public static void secondTask(int a) {
         if (a > 0){
             System.out.println("Correct");
         } else
             System.out.println("Incorrect");
     }
+
 }
