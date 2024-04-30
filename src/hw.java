5e444bb78a17acee79ae6e3302557de08a3a0436
Vladyslav Pudov <vladpudov2009@gmail.com>
2024-04-30 10:16:22 +0200

    fourth task

--- src/hw.java
+++ src/hw.java
@@ -2,7 +2,8 @@ public class hw {
     public static void main(String[] args) {
 //        firstTask(1);
 //        secondTask(-1);
-        thirdTask(1);
+//        thirdTask(1);
+//        fourthTask(1);
     }
 
     public static void firstTask(int a) {
@@ -21,6 +22,13 @@ public class hw {
 
     public static void thirdTask(int a) {
         if (a < 0) {
+            System.out.println("Correct");
+        } else
+            System.out.println("Incorrect");
+    }
+
+    public static void fourthTask(int a) {
+        if (a > 0 || a == 0){
             System.out.println("Correct");
         } else
             System.out.println("Incorrect");
